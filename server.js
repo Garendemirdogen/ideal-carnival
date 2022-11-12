e677bf68895294ad028f97fa721f27571864e646
Garen <garendemirdogen1@gmail.com>
2022-11-12 10:40:29 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const path = require("path");
 const express = require("express");
-
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const helpers = require("./utils/helpers");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: "Super secret secret",
+  cookie: { maxAge: 60000 * 10 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
